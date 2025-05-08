d6e565b5f8d01bccf8e8b53936ecbafc250a2199
Samuel-Rundell <runsamuelt@gmail.com>
2025-05-08 03:02:42 -0500

    route fix

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const mainRouter = require("./routes/index");
-
 const app = express();
-
 const { PORT = 3001 } = process.env;
+const mainRouter = require("./routes/index");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -25,7 +23,3 @@ app.use("/", mainRouter);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-module.exports.createClothingItem = (req) => {
-  console.log(req.user._id);
-};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const clothingItem = require("./clothingItems");
 const userRouter = require("./users");
 
 router.use("/users", userRouter);
-router.use("/users", clothingItem);
+router.use("/items", clothingItem);
 
 module.exports = router;
