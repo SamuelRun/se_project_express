d83842e6241d53e3415392c10f6cfa78c4aea24e
Samuel-Rundell <runsamuelt@gmail.com>
2025-05-20 11:42:18 -0500

    test_enpoints fix 13

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { getCurrentUser, updateCurrentUser } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
 router.use(auth);
 router.get("/users/me", getCurrentUser);
