f9b1e933752ecf771436a18d15144e05190d4fd5
Samuel-Rundell <runsamuelt@gmail.com>
2025-05-20 11:07:20 -0500

    test_enpoints fix 10

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -3,7 +3,6 @@ const {
   BadRequestError,
   NotFoundError,
   InternalServerError,
-  ForbiddenError,
 } = require("../utils/errors");
 
 const createItem = (req, res) => {
@@ -49,33 +48,26 @@ const deleteItem = (req, res) => {
   const { itemId } = req.params;
 
   ClothingItem.findById(itemId)
-    .orFail()
     .then((item) => {
-      if (item.owner.toString() !== req.user._id) {
-        throw new ForbiddenError(
-          "You don't have permission to delete this item"
-        );
-      }
       if (!item) {
         return res.status(404).json({ message: "Item not found" });
       }
-      return ClothingItem.findByIdAndDelete(itemId);
+      if (item.owner.toString() !== req.user._id) {
+        return res
+          .status(403)
+          .json({ message: "You don't have permission to delete this item" });
+      }
+      return ClothingItem.findByIdAndDelete(itemId).then((deletedItem) =>
+        res.status(200).json(deletedItem)
+      );
     })
-    .then((deletedItem) => res.status(200).json(deletedItem))
     .catch((err) => {
       console.error(err);
       if (err.name === "CastError") {
-        return res
-          .status(BadRequestError.statusCode)
-          .json({ message: "Invalid item ID" });
-      }
-      if (err.name === "DocumentNotFoundError") {
-        return res
-          .status(NotFoundError.statusCode)
-          .json({ message: "Item not found" });
+        return res.status(400).json({ message: "Invalid item ID" });
       }
       return res
-        .status(InternalServerError.statusCode)
+        .status(500)
         .json({ message: "An error occurred on the server" });
     });
 };
